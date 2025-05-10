613112a99808fb9cdf943f169f378d716706606e
TheZetmix <phoenicso2@gmail.com>
2025-05-10 06:47:15 +0000

    хз

--- examples/ex2.asm
+++ examples/ex2.asm
@@ -1,3 +1,35 @@
+section .func
+convert_int:
+    pusha
+    arg SI
+    int 0x30
+    mov DX SI
+    add DX BP
+    mov EAX 10
+    sub BP 1    
+    pow EAX BP
+    mov CX SI
+convert_int_loop:
+    load ECX CX
+    mul ECX EAX
+    add EBX ECX
+    div EAX 10
+    inc CX
+    cmp CX DX
+    jne convert_int_loop
+    define convert_result EBX
+    popa
+    ret
+endsec
+    
 section .main
     int 0x00
-    mov AX $q
+    dump 0x00 1
+    dump 0x01 2
+    dump 0x02 5
+    dump 0x03 3
+    dump 0x04 1
+    dump 0x05 2
+    dump 0x06 3
+    dump 0x07 0xEF
+    call convert_int 0x00
